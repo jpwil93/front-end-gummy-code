649a333fa74e114c6337a0f623b32acbe47892e5
SoapyPigeons <jpwil93@icloud.com>
2019-12-08 16:56:31 -0700

    built sign in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="sign-in">sign in</div>;
+  }
+}
+
+export default Signin;
