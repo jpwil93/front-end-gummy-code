b2842c2bf8ade72429b96063b4f51dc325ba5884
SoapyPigeons <jpwil93@icloud.com>
2019-12-08 16:24:49 -0700

    built layout grid and header navbar components

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <img src="http://via.placeholder.com/50x50" />
+      </div>
+    );
+  }
+}
---
+++ src/components/headernavbar/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="navbar"></div>;
+  }
+}
