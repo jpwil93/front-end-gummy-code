e5291c4e7acc81cafdef0ebc63cd24a6857041a3
SoapyPigeons <jpwil93@icloud.com>
2019-12-11 14:16:36 -0700

    built sign in form component for sign in component

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { reduxForm, Field } from "redux-form";
+import SignInForm from "./signinForm";
 
 class SignIn extends Component {
   constructor(props) {
@@ -10,12 +10,12 @@ class SignIn extends Component {
   }
 
   render() {
-    return <div className="sign-in">sign in</div>;
+    return (
+      <div className="sign-in">
+        <SignInForm className="sign-in__form" />
+      </div>
+    );
   }
 }
-
-SignIn = reduxForm({
-  form: "SignIn"
-})(SignIn);
 
 export default SignIn;
---
+++ src/components/auth/signinForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import { reduxForm, Field } from "redux-form";
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const { className } = this.props;
+    return <form className={`${className} sign-in-form`}>sign in</form>;
+  }
+}
+
+SignInForm = reduxForm({
+  form: "SignInForm"
+})(SignInForm);
+
+export default SignInForm;
