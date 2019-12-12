12bcc8fe0357cb87ffc83924d21cba7d198ae341
SoapyPigeons <jpwil93@icloud.com>
2019-12-12 14:22:37 -0700

    built header navbar reducer

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 
 export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   render() {
     return <div className="navbar"></div>;
   }
