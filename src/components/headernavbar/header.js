ce193517b00276062c86641cbe51c0f0fa8a433d
SoapyPigeons <jpwil93@icloud.com>
2019-12-14 11:51:15 -0700

    styled header and navbar components

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -3,19 +3,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="header">
-        <img src="http://via.placeholder.com/50x50" />
-        <div>
-          {" "}
-          className='header__links'
+        <img className="header__img" src="http://via.placeholder.com/50x50" />
+        <div className="header__links">
           {this.props.headerLinks.map((link, index) => {
             return (
               <a
