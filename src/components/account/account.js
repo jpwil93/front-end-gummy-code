06b51f484d5f5577312d53ad37049e2acf969ed9
SoapyPigeons <jpwil93@icloud.com>
2019-12-14 13:01:56 -0700

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -8,6 +8,18 @@ import PurchaseHistory from "./purchaseHistory";
 
 class Account extends Component {
   componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: "Shop",
+        path: "/shop"
+      },
+      {
+        _id: 1,
+        title: "Logout",
+        path: "/"
+      }
+    ];
     const navbarLinks = [
       {
         _id: 0,
@@ -22,7 +34,7 @@ class Account extends Component {
         component: <AccountInformation />
       }
     ];
-    this.props.setHeaderLinks([]);
+    this.props.setHeaderLinks(headerLinks);
     this.props.setNavbarLinks(navbarLinks);
   }
 
