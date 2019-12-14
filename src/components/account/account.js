e8826f28394c732014204858573ef77648ed4459
SoapyPigeons <jpwil93@icloud.com>
2019-12-14 12:01:56 -0700

    built base structure for account components

---
+++ src/components/account/account.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Account extends Component {
+  renderContent() {
+    return <h1> content </h1>;
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Account;
