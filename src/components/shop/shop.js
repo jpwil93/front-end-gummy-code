2a143b5267438feb6a03bbf18ef6f96e5d4d3887
SoapyPigeons <jpwil93@icloud.com>
2019-12-19 14:59:03 -0700

    built base for shop component and shop route

---
+++ src/components/shop/shop.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+class Shop extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: "Login",
+        path: "/signin"
+      }
+    ];
+    this.props.setHeaderLinks(headerLinks);
+
+    // fetch shop products action creator
+    // set header links
+    // set nav bar links
+  }
+
+  render() {
+    return (
+      <div className="shop">
+        {/* shop search bar component */}
+        {/* product component */}
+        {/* cart button */}
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return { state };
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
