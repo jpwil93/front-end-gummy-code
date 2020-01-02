5d985c85910d20a908c1a075df98d9d2647845f7
SoapyPigeons <jpwil93@icloud.com>
2020-01-02 14:20:10 -0700

    review form grid

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Router, Switch, Route } from "react-router-dom";
-
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(
@@ -17,11 +16,13 @@ const createStoreWithMiddleware = applyMiddleware()(
 import "./style/main.scss";
 
 import history from "./history";
+
 import Layout from "./components/layout";
 import Signin from "./components/auth/signin";
 import Signup from "./components/auth/signup";
 import Account from "./components/account/account";
 import Shop from "./components/shop/shop";
+import Review from "./components/order/review";
 
 function main() {
   ReactDOM.render(
@@ -31,11 +32,12 @@ function main() {
           <Switch>
             <Route path="/" exact component={Signin} />
             <Route path="/signin" exact component={Signin} />
-
             <Route path="/signup" exact component={Signup} />
+
             <Route path="/account" exact component={Account} />
 
             <Route path="/shop" exact component={Shop} />
+            <Route path="/order/review" exact component={Review} />
           </Switch>
         </Layout>
       </Router>
