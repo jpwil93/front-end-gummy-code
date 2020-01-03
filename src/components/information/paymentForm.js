3a281783ea7687bf720d49948bc9d2eb4a5eaeec
SoapyPigeons <jpwil93@icloud.com>
2020-01-03 15:51:42 -0700

    built payment form

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -1,82 +1,66 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { reduxForm, Field } from "redux-form";
+import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton } from '../formFields';
 
-import history from "../../history";
+import history from '../../history';
+import OrderSummary from './orderSummary';
 
 class PaymentForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-        <Field
-          className="sign-up-form__name"
-          type="name"
-          title="Name"
-          placeholder="Name"
-          name="name"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__address"
-          type="address"
-          title="Street Address"
-          placeholder="Street Address"
-          name="address"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__city"
-          type="city"
-          title="City"
-          placeholder="City"
-          name="city"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__state"
-          type="state"
-          title="State"
-          placeholder="State"
-          name="state"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__zipcode"
-          type="zipcode"
-          title="Zipcode"
-          placeholder="Zipcode"
-          name="zipcode"
-          component={FormInput}
-        />
 
-        <div className="sign-up-form__line"></div>
-        <Field
-          className="sign-up-form__use-this-address"
-          onClick={() => history.push("/information/payment")}
-          type="submit"
-          title="Use This Address"
-          name="use-this-address"
-          component={FormButton}
-        />
-        <Field
-          className="sign-up-form__back"
-          onClick={() => history.push("/signin")}
-          type="button"
-          title="Back"
-          name="back"
+    return (
+      <form onSubmit={handleSubmit} className={`${className} payment-form`}>
+        <Field className='payment-form__name'
+          type='name'
+          title='Name on Credit Card'
+          placeholder='Name'
+          name='name'
+          component={FormInput} />
+        <Field className='payment-form__card'
+          type='card'
+          title='Credit Card Number'
+          placeholder='____-____-____-____'
+          name='card'
+          component={FormInput} />
+
+        <Field className='payment-form__expiration'
+          type='expiration'
+          title='Expiration Date'
+          placeholder='expiration'
+          name='expiration'
+          component={FormInput} />
+        <Field className='payment-form__ccv'
+          type='ccv'
+          title='CCV'
+          placeholder='CCV'
+          name='ccv'
+          component={FormInput} />
+
+        <div className='payment-form__line'></div>
+        <Field className='payment-form__pay-complete'
+          onClick={() => history.push('/information/payment')}
+          type='submit'
+          title='Pay & Complete'
+          name='pay-complete'
+          component={FormButton} />
+        <Field className='payment-form__back'
+          onClick={() => history.push('/signin')}
+          type='button'
+          title='Back'
+          name='back'
           short={true}
-          component={FormButton}
-        />
+          component={FormButton} />
+        <OrderSummary className='payment-form__order-summary' />
       </form>
-    );
+    )
   }
 }
 
 PaymentForm = reduxForm({
-  form: "PaymentForm"
+  form: 'PaymentForm'
 })(PaymentForm);
 
 export default PaymentForm;
